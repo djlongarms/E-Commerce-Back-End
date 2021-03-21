8a877adaf967e81b8995532dd9241e4e769a4a29
Nick Palmer <ylnickpalmer@gmail.com>
2021-03-21 01:23:10 -0700

    fixed models, added packages, add category routes

--- models/index.js
+++ models/index.js
@@ -11,10 +11,16 @@ Product.belongsTo(Category, { foreignKey: 'category_id' })
 Category.hasMany(Product, { foreignKey: 'category_id' })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag, { foreignKey: 'product_id' })
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, { foreignKey: 'tag_id' })
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
