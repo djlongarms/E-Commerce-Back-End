7ed54a328ad139c8fc27f8cbba808292a502e092
Nick Palmer <ylnickpalmer@gmail.com>
2021-03-23 23:11:17 -0700

    finished server code, added README text

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # E-Commerce-Back-End
+
+This application allows a user to create products, tags to describe those products, and categories in which those products fall.
+
+Categories can be added by providing a name.
+
+Products can be added by providing a name, a price, and a stock.
+
+Tags can be added by providing a name.
+
+Tags and Products are connected to each other through ProductTag.
+
+Here is a link to a video showcasing the use of the application: [!Video](https://drive.google.com/file/d/1VldnGj3QralEwk9-2soa9LiCoHRH3e0_/view)
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
+
 // import sequelize connection
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_URL)
 
 const app = express()
 
@@ -9,4 +14,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-app.listen(process.env.PORT || 3000)
+sequelize.sync()
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(err => console.log(err))
